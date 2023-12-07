f9e3ed22ced24b6519e6302fe6de00d75a885845
sublime247 <kushimobashir2000@gmail.com>
2023-12-07 11:46:03 +0100

    added model to the project

--- kushimoproject/settings.py
+++ kushimoproject/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-     'portfolio.apps.PortfolioConfig',
+    'portfolio.apps.PortfolioConfig',
 ]
 
 MIDDLEWARE = [
