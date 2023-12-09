a8b1017ba5022be1294492a8cbc897a231148746
sublime247 <kushimobashir2000@gmail.com>
2023-12-10 00:12:07 +0100

    day2 learning

--- portfolio/models.py
+++ portfolio/models.py
@@ -6,11 +6,35 @@ class Project(models.Model):
     description = models.TextField(null=True, blank=True)
     proj_link = models.TextField(max_length=2000, blank=True, null=True)
     source_link = models.TextField(max_length=2000, blank=True, null=True)
+    tags = models.ManyToManyField('Tag', blank=True)
+    vote_totals = models.IntegerField(default=0, blank= True, null= True)
+    vote_ratio = models.IntegerField(default=0 ,blank= True, null= True)
     added_time = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
     
     
     def __str__(self): 
-        return self.title
+        return self.title  
+
+class Review(models.Model):
+    # owners =
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    Vote_Type = (
+        ('up', 'Up Vote'),
+        ('down', 'down Vote')
+    )
+    body =models.TextField(null=True, blank=True)
+    value = models.CharField(max_length=200, choices=Vote_Type)
+    added_time = models.DateTimeField(auto_now_add=True)
+
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
     
+    def __str__(self):
+       return  self.value
         
+class Tag(models.Model):
+      name = models.CharField(max_length=200)
+      added_time = models.DateTimeField(auto_now_add=True)
+      id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+      def __str__(self):
+         return  self.name
