61050d869c483c11b8b587d1234b61d46b0069ef
sublime247 <kushimobashir2000@gmail.com>
2023-12-10 18:42:25 +0100

    day3

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.projects, name='projects'),
     path('project1/<str:pk>/', views.project1, name='project1'),
+    path('create-project/', views.createProject, name= 'create-project')
     # path('admin/', admin.site.urls),
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -3,24 +3,23 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 from .models import Project
+from .forms import ProjectForm
 
-project_list = [
-    {"id": '1', "title": "Project A", "name": "Alpha"},
-    {"id": '2', "title": "Project B", "name": "Beta"},
-    {"id": '3', "title": "Project C", "name": "Charlie"},
-    {"id": '4', "title": "Project D", "name": "Delta"},
-    {"id": '5', "title": "Project E", "name": "Echo"}
-]
+
 
 def projects(request):
     project = Project.objects.all()
-    # title = 'Not tired'
-    # name = 'ADEOLA'
-    # num = 10
     context = { 'projlst': project}
     return render(request,  'temps1.html', context, )
+
 def project1(request, pk):
     projectObj = Project.objects.get(id=pk)
     tag =projectObj.tags.all()
     return render(request,   'temps2.html', {'project': projectObj, 'tags': tag} )
+
+
+def createProject(request):
+    form = ProjectForm()
+    context = {'form':form}
+    return render(request, 'project_form.html', context)
 
