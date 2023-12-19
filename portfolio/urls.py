dccec2394cf8f2df40c9b30ae19da60a8f512447
sublime247 <kushimobashir2000@gmail.com>
2023-12-19 15:12:01 +0100

    learnign rendering of template

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -5,6 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.projects, name='projects'),
     path('project1/<str:pk>/', views.project1, name='project1'),
-    path('create-project/', views.createProject, name= 'create-project')
+    path('create-project/', views.createProject, name='create-project'),
+    path('update-project/<str:pk>/', views.updateProject, name= 'update-project'),
+    path('delete-project/<str:pk>/', views.deleteProject, name= 'delete-project')
     # path('admin/', admin.site.urls),
 ]
